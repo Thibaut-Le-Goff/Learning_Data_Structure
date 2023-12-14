f002aa8af99af63a106e292be822d074d0ce50b0
Thibaut-Le-Goff <thibaut.le.goff27@gmail.com>
2023-12-14 16:03:14 +0100

    modifié :         output.txt 	modifié :         src/linked_list/crud/delete.c

--- src/linked_list/crud/delete.c
+++ src/linked_list/crud/delete.c
@@ -11,8 +11,6 @@ void parent_node_case(Node *linked_list_iterator_node);
 
 void delete_node(Node *linked_list_first_element, int hash_node_to_delete) {
 
-    //int num_max_jumps = 10;
-    // 
     Node *linked_list_iterator_node = linked_list_first_element;
 
     for (int jump_thought_nodes = 0; jump_thought_nodes <= NUM_MAX_JUMPS; ++jump_thought_nodes)
@@ -57,12 +55,14 @@ void delete_node(Node *linked_list_first_element, int hash_node_to_delete) {
 
         if (jump_thought_nodes == NUM_MAX_JUMPS) 
         {
-            fprintf(stderr, "Error : The node wanted was not found in %d jumps.", NUM_MAX_JUMPS + 1);
+            fprintf(stderr, "Error : The node wanted was not found in %d jumps.", NUM_MAX_JUMPS);
         }
     }
 }
 
-// -----------------------------------------------------------------------------------------------------------
+
+// ---------------------------------------- leaf_node_case -------------------------------------------------------------------
+
 
 void leaf_node_case(Node *node_to_delete)
 {
@@ -90,69 +90,121 @@ void leaf_node_case(Node *node_to_delete)
     }
 }
 
-// -----------------------------------------------------------------------------------------------------------
+
+// --------------------------------------- parent_node_case --------------------------------------------------------------------
+
+
+void from_greater_to_lesser_path(Node *node_to_delete);
+void from_lesser_to_greater_path(Node *node_to_delete);
 
 void remplacement_of_node(Node *node_to_delete, Node *remplacement_node);
-/**/
+
 void parent_node_case(Node *node_to_delete)
 {
     printf("Node parent à suprimer %p", (void *)node_to_delete);
 
     /**/
     // The node to replace the node to delete must be the next higher one
-    if (node_to_delete->node_greater_hash == NULL)
-    // if there are no node with a greater hash than the node to delete
-    // the 
-    {
-        Node *remplacement_node = node_to_delete->node_lesser_hash;
-        remplacement_of_node(node_to_delete, remplacement_node);
-    }
-    else if (node_to_delete->node_greater_hash->node_lesser_hash == NULL)
-    // if the node on the greater branch,of the node to delete, does not 
-    // have a lesser child that mean the next higher hash is this one
-    {
-        Node *remplacement_node = node_to_delete->node_greater_hash;
-        remplacement_of_node(node_to_delete, remplacement_node);
-    }
-    else if (node_to_delete->node_greater_hash->node_lesser_hash != NULL)
-    // if the node on the greater branch,of the node to delete
-    // have a lesser child that mean the next higher hash is the
-    // one of the lesser 
-    {
-        //int num_max_jumps = 10;
-        Node *linked_list_iterator_node = node_to_delete->node_greater_hash;
+    if (node_to_delete->node_lesser_hash == NULL)
+    // if the node to delete, does not 
+    // have a lesser child that mean the next higher hash is the one 
+    // on the greater branche or the lesser one 
 
-        for (int jump_thought_nodes = 0; jump_thought_nodes <= NUM_MAX_JUMPS; ++jump_thought_nodes)
-        // we will iterate through the nodes by the lesser path because 
-        // the less hash value in this path is the next greater hash after 
-        // the hash to delete
+    // if the node on the greater branch, of the node to delete
+    // have a lesser child that mean the next higher hash is the
+    // one with the lesser hash on the lesser path
+    {
+        from_greater_to_lesser_path(node_to_delete);
+    }
+    else if (node_to_delete->node_greater_hash == NULL)
+    // if the node to delete, does not 
+    // have a greater child that mean the next higher hash is the one 
+    // on the greater branche or the lesser one 
+
+    // if the node on the greater branch, of the node to delete
+    // have a lesser child that mean the next higher hash is the
+    // one with the lesser hash on the lesser path
+    {
+        from_lesser_to_greater_path(node_to_delete);
+    }
+}
+
+
+// --------------------------------------- from_greater_to_lesser_path --------------------------------------------------------------------
+
+
+void from_greater_to_lesser_path(Node *node_to_delete) {
+        
+    Node *linked_list_iterator_node = node_to_delete->node_greater_hash;
+
+    for (int jump_thought_nodes = 0; jump_thought_nodes <= NUM_MAX_JUMPS; ++jump_thought_nodes)
+    // we will iterate through the nodes by the lesser path because 
+    // the less hash value in this path is the next greater hash after 
+    // the hash to delete (because we are starting by its greater branche)
+    {
+        if (linked_list_iterator_node->node_lesser_hash == NULL)
+        // if there are no lesser hash, the iterator node 
+        // is considered to be the node of remplacement
         {
-            if (linked_list_iterator_node->node_lesser_hash == NULL)
-            // if there are no lesser hash, the iterator node 
-            // is considered to be the node of remplacement
-            {
-                Node *remplacement_node = linked_list_iterator_node;
-                remplacement_of_node(node_to_delete, remplacement_node);
-            }
-            else if (linked_list_iterator_node->node_lesser_hash != NULL)
-            // if there are a lesser hash, the iterator node 
-            // is considered to have a hash value greater than 
-            // another node, this is not the next greater node 
-            // of the node to delete, the iteration continu
-            {
-                linked_list_iterator_node = linked_list_iterator_node->node_lesser_hash;
-            }
-            
-            if (jump_thought_nodes == NUM_MAX_JUMPS) 
-            {
-                fprintf(stderr, "Error : The node wanted was not found in %d jumps.", NUM_MAX_JUMPS + 1);
-            }
+            Node *remplacement_node = linked_list_iterator_node;
+            remplacement_of_node(node_to_delete, remplacement_node);
+            break;
+        }
+        else if (linked_list_iterator_node->node_lesser_hash != NULL)
+        // if there are a lesser hash, the iterator node 
+        // is considered to have a hash value greater than 
+        // another node, this is not the next greater node 
+        // of the node to delete, the iteration continu
+        {
+            linked_list_iterator_node = linked_list_iterator_node->node_lesser_hash;
+        }
+         
+        if (jump_thought_nodes == NUM_MAX_JUMPS)
+        {
+            fprintf(stderr, "Error : The node wanted was not found in %d jumps.", NUM_MAX_JUMPS);
         }
     }
 }
 
 
+// --------------------------------------- from_lesser_to_greater_path --------------------------------------------------------------------
 
+
+void from_lesser_to_greater_path(Node *node_to_delete) {
+        
+    Node *linked_list_iterator_node = node_to_delete->node_lesser_hash;
+
+    for (int jump_thought_nodes = 0; jump_thought_nodes <= NUM_MAX_JUMPS; ++jump_thought_nodes)
+    // we will iterate through the nodes by the lesser path because 
+    // the less hash value in this path is the next greater hash after 
+    // the hash to delete (because we are starting by its greater branche)
+    {
+        if (linked_list_iterator_node->node_greater_hash == NULL)
+        // if there are no lesser hash, the iterator node 
+        // is considered to be the node of remplacement
+        {
+            Node *remplacement_node = linked_list_iterator_node;
+            remplacement_of_node(node_to_delete, remplacement_node);
+            break;
+        }
+        else if (linked_list_iterator_node->node_greater_hash != NULL)
+        // if there are a lesser hash, the iterator node 
+        // is considered to have a hash value greater than 
+        // another node, this is not the next greater node 
+        // of the node to delete, the iteration continu
+        {
+            linked_list_iterator_node = linked_list_iterator_node->node_greater_hash;
+        }
+         
+        if (jump_thought_nodes == NUM_MAX_JUMPS) 
+        {
+            fprintf(stderr, "Error : The node wanted was not found in %d jumps.", NUM_MAX_JUMPS);
+        }
+    }
+}
+
+
+// --------------------------------------- remplacement_of_node --------------------------------------------------------------------
 
 
 void remplacement_of_node(Node *node_to_delete, Node *remplacement_node) {
@@ -161,7 +213,9 @@ void remplacement_of_node(Node *node_to_delete, Node *remplacement_node) {
     if ( (node_to_delete->node_lesser_hash != NULL) && 
          (node_to_delete->node_lesser_hash != remplacement_node) )
     // if the node to delete have a lesser child
-    // if the node of replacement have a sister
+    // who is not the node of replacement 
+    // (to prevent the node to delete to give a pointer who 
+    // point to the greater child to itself)
     {
         remplacement_node->node_lesser_hash = node_to_delete->node_lesser_hash;
         // the sister becam his lesser child
@@ -174,7 +228,7 @@ void remplacement_of_node(Node *node_to_delete, Node *remplacement_node) {
     if ( (node_to_delete->node_greater_hash != NULL) && 
          (node_to_delete->node_greater_hash != remplacement_node) )
     // if the node to delete has a greater child 
-    // who is not the node to delete 
+    // who is not the node of replacement 
     // (to prevent the node to delete to give a pointer who 
     // point to the greater child to itself)
     {
@@ -198,4 +252,7 @@ void remplacement_of_node(Node *node_to_delete, Node *remplacement_node) {
         
         node_to_delete->parent_node = NULL;
     }
+    //if else (node_to_delete->parent_node == NULL)
+    // if the node to delete is the root node
+    //{}
 }
