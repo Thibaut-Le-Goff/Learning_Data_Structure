6cdbf746c262d7067214707023e2a6952da35375
Thibaut Le Goff <thibaut.le.goff27@gmail.com>
2024-04-15 22:09:14 +0200

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-rm -r bin
+bin_path="bin"
+
+
+if [ -e "$bin_path" ]; then
+    rm -r bin
+else
+    echo "There is no binary files."
+fi
+
 make 
 #make valgrind > logs/outputs/output_valgrind.txt 2> logs/errors/error_valgrind.txt
 make sanitize 
